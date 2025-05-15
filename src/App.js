648469aa5e302e3f0627e56f773b8404e025f698
Anant Jindal <anant.jindal@kredmint.com>
2025-05-15 12:48:02 +0530

    Created necessary routes for the application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Details from './pages/Details';
+import Login from './pages/Login';
+
 function App() {
   return (
-    <div>Hello world</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/details/:id' element={<Details />} />
+    </Routes>
   );
 }
 
