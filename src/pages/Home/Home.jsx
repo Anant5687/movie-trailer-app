b137a2bac9b8ad2b4c6ab280c2a28b7b4f342203
Anant Jindal <anant.jindal@kredmint.com>
2025-05-28 12:39:13 +0530

    Added search bar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,10 +5,15 @@ import Card from '../../components/card/card';
 
 const Home = () => {
   return (
-    <div className="home_page">
-      {[1, 2, 3, 4, 5, 6].map((ele, i) => (
-        <Card key={i} />
-      ))}
+    <div className="container">
+      <div className='search-bar'>
+        <input type="text" className='text-bar' placeholder='Search Your Movie' />
+      </div>
+      <div className="home_page">
+        {[1, 2, 3, 4, 5, 6].map((ele, i) => (
+          <Card key={i} />
+        ))}
+      </div>
     </div>
   );
 };
