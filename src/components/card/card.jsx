43b3802dc76a55de12667ebe2def2b94b2d9165f
Anant Jindal <anant.jindal@kredmint.com>
2025-05-28 12:25:11 +0530

    Created card images

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -7,11 +7,18 @@ import './style.css';
 const Card = () => {
   return (
     <article className="movie_card">
-      <a href="#">
-        <div>
-          <img src={MovieLogo} alt="MovieLogo" />
+      <a href="/details/8798">
+        <div className="img_container">
+          <img src={MovieLogo} alt="MovieLogo" className="img" />
         </div>
       </a>
+      <header>
+        <h2 className='movie_name'>
+          <a href="/details/8798" className='link'>
+            {`Download Peaky Blinders (Season 1 – 4) Dual Audio {Hindi-English} WEB Series 480p | 720p | 1080p BluRay ESub`}
+          </a>
+        </h2>
+      </header>
     </article>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-import './style.css'
-import Card from '../../components/card/card'
+import './style.css';
+import Card from '../../components/card/card';
 
 const Home = () => {
   return (
-    <div className='home_page'>
-      <Card />
+    <div className="home_page">
+      {[1, 2, 3, 4, 5, 6].map((ele, i) => (
+        <Card key={i} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
